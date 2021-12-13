d57e428f6a0aa9500b429c7d4615e8e77b05863a
gormlessfie <gmlessfie@gmail.com>
2021-12-12 16:18:06 -0800

    Add errors to create form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     if @article.save
-      redirect_to @article
+      redirect_to article_path(@article)
     else
       render :new
     end
